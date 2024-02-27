f885f7c2794044188df1b34631907bdeef8470e0
MikeeOo <michal.olszewskki@gmail.com>
2024-02-27 16:30:32 +0100

    created HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+import { JSX } from "react";
+
+export default function HomePage(): JSX.Element {
+    return <div className="h-screen bg-black">Home</div>;
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,17 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
+import HomePage from "./pages/HomePage.tsx";
 import App from "./App.tsx";
 
 export const router = createBrowserRouter([
     {
-        path : "/",
-        element: <App/>
-    }
-])
-
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />,
+            },
+        ],
+    },
+]);
