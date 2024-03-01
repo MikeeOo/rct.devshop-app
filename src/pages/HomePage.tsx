4f46fed9171d6cfd80708db9a968f83a72bde0a6
MikeeOo <michal.olszewskki@gmail.com>
2024-03-01 09:43:33 +0100

    mapped fake data to display products

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,15 @@
 import { JSX } from "react";
+import { products } from "../../ignore/products.ts";
+import Card from "../partials/Card.tsx";
 
 export default function HomePage(): JSX.Element {
-    return <div className="h-screen bg-black">Home</div>;
+    return (
+        <>
+            <div className="grid min-h-screen grid-cols-4">
+                {products.map((product) => (
+                    <Card key={product.id} product={product} />
+                ))}
+            </div>
+        </>
+    );
 }
