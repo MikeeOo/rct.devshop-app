2a1d35991ee046444bf3a0033ffc5c0ed6210045
MikeeOo <michal.olszewskki@gmail.com>
2024-03-01 09:47:03 +0100

    added product page component to router file

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import HomePage from "./pages/HomePage.tsx";
 import App from "./App.tsx";
+import ProductPage from "./pages/ProductPage.tsx";
 
 export const router = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
             {
                 index: true,
                 element: <HomePage />,
+            },
+            {
+                path: "/products/:id",
+                element: <ProductPage />,
             },
         ],
     },
