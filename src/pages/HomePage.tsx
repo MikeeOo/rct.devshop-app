a82abfce9f5afc0ecd01efc34aacecdae32d585a
MikeeOo <michal.olszewskki@gmail.com>
2024-03-07 22:00:09 +0100

    fetched all products from api

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,17 @@
-import { JSX } from "react";
-import { products } from "../../ignore/products.ts";
+// @ts-nocheck
+import { JSX, useEffect, useState } from "react";
 import Card from "../partials/Card.tsx";
+import { API_URL } from "../data/constants.ts";
 
 export default function HomePage(): JSX.Element {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(`${API_URL}/products`)
+            .then((res) => res.json())
+            .then((data) => setProducts(data));
+    }, []);
+
     return (
         <>
             <div className="grid min-h-screen grid-cols-4">
